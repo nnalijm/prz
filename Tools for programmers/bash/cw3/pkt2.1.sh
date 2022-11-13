c115e871c6e2507b0bfcc0ef63e1f656e0286972
nnalijm <naaaaaaaaaaaad@gmail.com>
2022-11-13 14:30:33 +0100

    pkt2.1 comments

--- Tools for programmers/bash/cw3/pkt2.1.sh
+++ Tools for programmers/bash/cw3/pkt2.1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Command 'test' using 'f' option checks does 'filename' exist or no
+
+# Polecenie 'test' z parametrem 'f' sprawdza, czy istenieje
+# plik nazwa ktorego(ściżka absolutna) jest podana jako argument 
+# przy urochomianiu skypta.
+# W przypadku braku plika lub niepoprawnej nazwy wyswietla 
+# komunikat 'error'
+# W dobrzym przypadku wyswietla komunikat 'Plik istnieje'
+ 
+
 if test -f $1 ; then
 	echo "Plik istnieje"
 else
